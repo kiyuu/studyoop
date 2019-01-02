9a0a0c11b069b190b7eff22130e9df1cd1a263aa
kiyu <xiukyxiglht@outlook.jp>
2019-01-02 23:15:42 +0900

    リファクタリング前

---
+++ StudyOOP/StudyOOP/Common/Settings.cs
@@ -0,0 +1,17 @@
+﻿namespace StudyOOP.Common
+{
+    using System.IO;
+
+    internal static class Settings
+    {
+        internal static string InputDirectory => Path.Combine(".", "input");
+
+        internal static string OutputDirectory => Path.Combine(".", "output");
+
+        internal static string InputFileExtension => ".dat";
+
+        internal static string OutputFileExtension => ".tsv";
+
+        internal static string TsvSeparater => "\t";
+    }
+}
---
+++ StudyOOP/StudyOOP/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace StudyOOP
+{
+    using StudyOOP.Convert;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Converter.ConvertWXXX5555ToEmployeeTSV();
+            Converter.ConvertWXXX6666ToItemTSV();
+        }
+    }
+}
