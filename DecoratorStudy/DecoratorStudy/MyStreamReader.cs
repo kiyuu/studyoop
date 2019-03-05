9b9ac242cbffce4c51d1025ed162fc9fe077c9dd
kiyu <xiukyxiglht@outlook.jp>
2019-03-05 16:53:52 +0900

    デコレーターパターン課題 StreamReaderBaseを作成

---
+++ DecoratorStudy/DecoratorStudy/MyStreamReader.cs
@@ -0,0 +1,69 @@
+﻿namespace DecoratorStudy
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    public class MyStreamReader : IDisposable
+    {
+        private Stream _stream;
+        private Encoding _encoding;
+        private bool _closable;
+
+        public MyStreamReader(Stream stream, Encoding encoding)
+        {
+            this._stream = stream;
+            this._encoding = encoding;
+        }
+
+        public MyStreamReader(string filePath, Encoding encoding)
+        {
+            this._stream = new FileStream(
+                filePath,
+                FileMode.Open,
+                FileAccess.Read,
+                FileShare.Read,
+                1024,
+                FileOptions.SequentialScan);
+
+            this._encoding = encoding;
+            this._closable = true;
+        }
+
+        internal bool LeaveOpen
+        {
+            get { return !this._closable; }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+        }
+
+        public string ReadToEnd()
+        {
+            // new StreamReader(Path.Combine(@".\", "input", "rss.xml"), Encoding.UTF8).ReadToEnd()で実装としないこと！！！！！！！！！
+            // this.Streamを使用して実装すること
+            throw new NotImplementedException();
+        }
+
+        protected virtual void Dispose(bool isDisposing)
+        {
+            try
+            {
+                if (!this.LeaveOpen && isDisposing && (this._stream != null))
+                {
+                    this._stream.Close();
+                }
+            }
+            finally
+            {
+                if (!this.LeaveOpen && (this._stream != null))
+                {
+                    this._stream = null;
+                    this._encoding = null;
+                }
+            }
+        }
+    }
+}
---
+++ DecoratorStudy/DecoratorStudy/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DecoratorStudy
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    /// <summary>
+    /// ファイル読み取り
+    /// </summary>
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write(ReadAllText(Path.Combine(@".\", "input", "rss.xml"), Encoding.UTF8));
+            Console.Write("何かキーを入力してください。");
+            Console.ReadKey();
+        }
+
+        public static string ReadAllText(string filePath, Encoding encoding)
+        {
+            using (var sr = new MyStreamReader(filePath, encoding))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+    }
+}
