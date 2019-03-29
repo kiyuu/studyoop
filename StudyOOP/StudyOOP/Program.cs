fce492007c3fe8a1d5493b4366e4a1b4c54e08c7
kiyu <xiukyxiglht@outlook.jp>
2019-03-29 09:22:37 +0900

    シンプルファクトリパターン実装

--- StudyOOP/StudyOOP/Program.cs
+++ StudyOOP/StudyOOP/Program.cs
@@ -1,19 +1,95 @@
 ﻿namespace StudyOOP
 {
+    using System;
     using System.Collections.Generic;
     using StudyOOP.Convert;
+
+    using System.IO;
+    using System.Xml.Linq;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            var converters = new List<FlatFileToTsvConverterBase>()
-                {
-                    new WXXX5555ToEmployeeTSVConverter(),
-                    new WXXX6666ToItemTSVConverter()
-                };
+            /////var converters = Factory.CreateInstances(InstanceGroupIDs.FlatFileToTsvConverters);
 
-            converters.ForEach(c => c.Execute());
+            var converters = Factory.CreateInstances("FlatFileToTsvConverters");
+
+            foreach (var converter in converters)
+            {
+                converter.Execute();
+            }
+
+            ////Factory.CreateInstance(InstanceIDs.EmployeeConverter).Execute();
+            ////Factory.CreateInstance(InstanceIDs.ItemConverter).Execute();
+            Factory.CreateInstance("EmployeeConverter").Execute();
+            Factory.CreateInstance("ItemConverter").Execute();
+        }
+
+        public class Factory
+        {
+            public static FlatFileToTsvConverterBase[] CreateInstances(string groupid)
+            {
+                FlatFileToTsvConverterBase[] flatFileToTsvConverters = new FlatFileToTsvConverterBase[0];
+
+                string sFile = Path.Combine(Directory.GetCurrentDirectory(), "Setting", "InstanceInfo.xml");
+
+                var xdoc = XDocument.Load(sFile);
+
+                var xelements = xdoc.Elements("Extension");
+
+                foreach (var row in xelements)
+                {
+                    foreach (var item in row.Elements("Group"))
+                    {
+                        if (item.Attribute("Id").Value == groupid)
+                        {
+                            foreach (var row2 in row.Elements("Instance"))
+                            {
+                                Array.Resize(ref flatFileToTsvConverters, flatFileToTsvConverters.Length + 1);
+
+                                // インスタンス配列を生成
+                                var type = Type.GetType(row2.Attribute("ClassName").Value);
+                                flatFileToTsvConverters[flatFileToTsvConverters.Length - 1] = (FlatFileToTsvConverterBase)Activator.CreateInstance(type);
+                            }
+                        }
+                    }
+                }
+
+                return flatFileToTsvConverters;
+            }
+
+            public static FlatFileToTsvConverterBase CreateInstance(string instanceid)
+            {
+                FlatFileToTsvConverterBase flatFileToTsvConverter = null;
+
+                string sFile = Path.Combine(Directory.GetCurrentDirectory(), "Setting", "InstanceInfo.xml");
+
+                var xdoc = XDocument.Load(sFile);
+
+                var xelements = xdoc.Elements("Extension");
+
+                foreach (var row in xelements)
+                {
+                    foreach (var item in row.Elements("Instance"))
+                    {
+                        if (item.Attribute("Id").Value == instanceid)
+                        {
+                            // インスタンスを生成
+                            var type = Type.GetType(item.Attribute("ClassName").Value);
+                            flatFileToTsvConverter = (FlatFileToTsvConverterBase)Activator.CreateInstance(type);
+                            break;
+                        }
+                    }
+
+                    if (flatFileToTsvConverter != null)
+                    {
+                        break;
+                    }
+                }
+
+                return flatFileToTsvConverter;
+            }
         }
     }
 }
